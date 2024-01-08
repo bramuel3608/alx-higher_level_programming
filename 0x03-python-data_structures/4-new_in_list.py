049672d2a3e67feb1708488e118d72577362200f
bramuel3608 <ochiengbramuel@gmail.com>
2024-01-08 05:17:17 -0800

    function replaces elements at a specific position

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/pyhton3
+def new_in_list(my_list, idx, element):
+    if idx < 0:
+        return (my_list)
+
+    if idx > (len(my_list) - 1):
+        return (my_list)
+    newlist = [i for i in my_list]
+    newlist[idx] = element
+    return (newlist)
